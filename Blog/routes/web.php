d87d3d96c5d9e5e5cad046f88f646a7067d52c9e
Rajin.p.k <52348554+rajinpklimenzy@users.noreply.github.com>
2021-11-17 11:55:02 +0530

    c1
    c1

--- Blog/routes/web.php
+++ Blog/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('test',function(){
+Route::get('test2',function(){
 
 echo "string";
 
